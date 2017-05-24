b72afc5d3a27c65321cf769cc7739a09387df4a7
kornerr <kornerr@kornerrs-iMac.local>
2017-05-24 07:29:23 -0700

    Add stub files to build osgNativeLib for iOS

---
+++ ios/src/osgNativeLib.cpp
@@ -0,0 +1,52 @@
+
+/*
+This file is part of OpenSceneGraph cross-platform guide:
+  https://github.com/OGStudio/openscenegraph-cross-platform-guide
+
+Copyright (C) 2017 Opensource Game Studio
+
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include "osgNativeLib.h"
+
+//#include "App.h"
+
+// Init OSG plugins.
+//USE_OSGPLUGIN(osg2)
+//USE_SERIALIZER_WRAPPER_LIBRARY(osg)
+
+// Create application instance.
+//App app;
+
+
+UIView *init(UIView *parent, int width, int height, float scale);
+{
+    // TODO.
+    return 0;
+}
+
+void frame()
+{
+    // TODO.
+}
+
+void loadModel(const std::string &path)
+{
+    // TODO.
+}
+
