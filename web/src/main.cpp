9ed24af880bdd2b6f76e7b27a7bcc12df2708f21
Michael Kapelko <kornerr@gmail.com>
2017-06-20 21:17:42 +0700

    Clarify main.cpp

--- web/src/main.cpp
+++ web/src/main.cpp
@@ -1,55 +1,55 @@
 
+#include <emscripten.h>
 #include <SDL2/SDL.h>
 
-#include "WebApplication.h"
-WebApplication *app = 0;
-#include <emscripten.h>
+//#include "Application.h"
+//Application *app = 0;
 
+// Stand alone function that is called by Emscripten to run the app.
 void loop()
 {
     SDL_Event e;
     while (SDL_PollEvent(&e))
     {
+        // TODO: Remove keyup/down.
         switch (e.key.keysym.sym)
         {
             case SDLK_UP:
-                printf("OSGSDL. Keyup\n");
+                printf("OSGWeb. Keyup\n");
                 break;
             case SDLK_DOWN:
-                printf("OSGSDL. Keydown\n");
+                printf("OSGWeb. Keydown\n");
                 break;
             default:
                 break;
         }
-        if (app)
-            app->handleEvent(e);
+        //app->handleEvent(e);
     }
-    if (app) {
-        app->frame();
-        //app->loadScene("box.osgt");
-    }
+    //app->frame();
 }
 
 int main(int argc, char *argv[])
 {
+    // Make sure SDL is working.
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        printf("OSGSDL. Could not init SDL: '%s'\n", SDL_GetError());
+        printf("OSGWeb. Could not init SDL: '%s'\n", SDL_GetError());
         return 1;
     }
+    // Clean SDL up at exit.
     atexit(SDL_Quit);
-    printf("OSGSDL. init\n");
+    // Configure rendering context.
     SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
     SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
     SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    // Rendering window.
+    // Create rendering window.
     int width = 800;
     int height = 600;
     SDL_Window* window =
         SDL_CreateWindow(
-            "OSGSDL",
+            "OSGWeb",
             SDL_WINDOWPOS_CENTERED,
             SDL_WINDOWPOS_CENTERED,
             width,
@@ -57,14 +57,17 @@ int main(int argc, char *argv[])
             SDL_WINDOW_OPENGL);
     if (!window)
     {
-        printf("OSGSDL. Could not create window: '%s'\n", SDL_GetError());
+        printf("OSGWeb. Could not create window: '%s'\n", SDL_GetError());
         return 1;
     }
     SDL_GL_CreateContext(window);
-    printf("OSGSDL. Created window and context\n");
-    app = new WebApplication;
+    // Create application.
+    /*
+    app = new Application;
     app->setEmbeddedWindowSize(width, height);
     app->loadScene("box.osgt");
+    */
+    // Render asynchronously.
     emscripten_set_main_loop(loop, -1, 0);
     return 0;
 }
